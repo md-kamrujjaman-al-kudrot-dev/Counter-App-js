a0740ec5cbb5b2879e05ef195dd185b0cd3599f2
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-08-19 11:56:15 +0600

    feat(counter): persist value using localStorage

--- src/script.js
+++ src/script.js
@@ -2,22 +2,57 @@ const box = document.getElementById("box")
 const remove_button = document.getElementById("remove_button")
 const add_button = document.getElementById("add_button")
 const reset_button = document.getElementById("reset_button")
+const maximum_part = document.getElementById("maximum")
 
 let count = 0
-function increase(){
-    count ++;
-    box.innerText = count;
+
+
+if(localStorage.getItem("data") !== ''){
+    showData();
+}else{
+    localStorage.setItem("data",0)
+    showData()
 }
 
-function dicrease(){
-    if(count > 0){
+function saveData() {
+    localStorage.setItem("data", count )
+}
+
+function showData() {
+    box.innerText = localStorage.getItem("data");
+    count = localStorage.getItem("data")
+}
+
+
+function increase() {
+    count++;
+    box.innerText = count;
+
+    if (count >= 10) {
+        count = 10;
+        box.innerText = count;
+        maximum_part.innerHTML = `<p  class="bg-red-400 font-extrabold p-1 rounded">Maximum limit reached</p>`
+    }
+    saveData()
+}
+
+function dicrease() {
+    if (count > 0) {
         count--;
+        maximum_part.innerHTML = ""
     }
     box.innerText = count;
+    saveData()
 }
 
 
-function reset(){
+
+
+function reset() {
     count = 0;
     box.innerText = count;
+    saveData()
+    // localStorage.setItem("counter", count)
+    
 }
+
